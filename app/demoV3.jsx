073710a035b8322a1ee0d1c6c29e217f721e50dd
skycloud1030 <skycloud1030@gmail.com>
2018-05-02 15:24:29 +0800

    Add Slide & Fade demo

--- app/demoV3.jsx
+++ app/demoV3.jsx
@@ -1,134 +1,168 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'react-bootstrap-carousel/dist/react-bootstrap-carousel.css';
-import {React_Bootstrap_Carousel} from 'react-bootstrap-carousel';
+import React from "react";
+import ReactDOM from "react-dom";
+import "react-bootstrap-carousel/dist/react-bootstrap-carousel.css";
+import { React_Bootstrap_Carousel } from "react-bootstrap-carousel";
 
-class Demo extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state={
-          autoplay:true
-        }
+const styles = { height: 400, width: "100%" };
+class Demo extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      autoplay: true
+    };
+  }
+  onSelect = (active, direction) => {
+    console.log(`active=${active} && direction=${direction}`);
+  };
+  slideNext = () => {
+    this.slider.slideNext();
+  };
+  slidePrev = () => {
+    this.slider.slidePrev();
+  };
+  goToSlide = () => {
+    this.slider.goToSlide(4);
+  };
+  autoplay = () => {
+    this.setState({ autoplay: !this.state.autoplay });
+  };
+  _changeIcon = () => {
+    let { leftIcon, rightIcon } = this.state;
+    if (leftIcon && rightIcon) {
+      this.setState({
+        leftIcon: undefined,
+        rightIcon: undefined
+      });
+    } else {
+      this.setState({
+        leftIcon: <span className="glyphicon glyphicon-glass" />,
+        rightIcon: <span className="glyphicon glyphicon-music" />
+      });
     }
-    onSelect= (active,direction)=>{
-        console.log(`active=${active} && direction=${direction}`);
-    }
-    slideNext=()=>{
-      this.slider.slideNext();
-    }
-    slidePrev=()=>{
-      this.slider.slidePrev();
-    }
-    goToSlide=()=>{
-      this.slider.goToSlide(4);
-    }
-    autoplay=()=>{
-      this.setState({autoplay:!this.state.autoplay});
-    }
-    _changeIcon=()=>{
-      let {leftIcon,rightIcon}=this.state;
-      if(leftIcon && rightIcon){
-        this.setState({
-          leftIcon:undefined,
-          rightIcon:undefined
-        });
-      }
-      else{
-        this.setState({
-          leftIcon:<span className="glyphicon glyphicon-glass"></span>,
-          rightIcon:<span className="glyphicon glyphicon-music"></span>
-        });
-      }
-    }
-    render() {
-      let {leftIcon,rightIcon}=this.state;
-      return(
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-md-12" style={{paddingTop:'20px'}}>
-              <button type="button" className="btn btn-primary" onClick={this._changeIcon}>
+  };
+  render() {
+    let { leftIcon, rightIcon } = this.state;
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-12" style={{ paddingTop: "20px" }}>
+            <div className="btn-group">
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={this._changeIcon}
+              >
                 Change Icon
-              </button>{' '}
-              <button type="button" className="btn btn-primary" onClick={this.slidePrev}>
+              </button>
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={this.slidePrev}
+              >
                 Slider prev
-              </button>{' '}
-              <button type="button" className="btn btn-primary" onClick={this.slideNext}>
+              </button>
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={this.slideNext}
+              >
                 Slider next
-              </button>{' '}
-              <button type="button" className="btn btn-primary" onClick={this.goToSlide}>
+              </button>
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={this.goToSlide}
+              >
                 Go to slide 4
-              </button>{' '}
-              <button type="button" className="btn btn-primary" onClick={this.autoplay}>
-                {this.state.autoplay?"Stop Autoplay":"Start Autoplay"}
+              </button>
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={this.autoplay}
+              >
+                {this.state.autoplay ? "Stop Autoplay" : "Start Autoplay"}
               </button>
             </div>
-            <div className="col-md-12" style={{paddingTop:'20px'}}>
-              <React_Bootstrap_Carousel
-                animation={true}
-                autoplay={this.state.autoplay}
-                slideshowSpeed={7000}
-                defaultActiveIndex={0}
-                leftIcon={leftIcon}
-                rightIcon={rightIcon}
-                onSelect={this.onSelect}
-                ref={r=>this.slider=r}
-                className="carousel-fade"
-              >
-                <div style={{height:400}}>
-                  <img
-                    style={{width:"100%",height:"100%"}}
-                    src="https://www.w3schools.com/bootstrap/ny.jpg"
+          </div>
+          <div className="col-md-12" style={{ marginTop: 20 }}>
+            <React_Bootstrap_Carousel
+              animation={true}
+              autoplay={this.state.autoplay}
+              slideshowSpeed={7000}
+              defaultActiveIndex={0}
+              leftIcon={leftIcon}
+              rightIcon={rightIcon}
+              onSelect={this.onSelect}
+              ref={r => (this.slider = r)}
+            >
+              <div style={{ height: 400 }}>
+                <img
+                  style={{ width: "100%", height: "100%" }}
+                  src="https://www.w3schools.com/bootstrap/ny.jpg"
+                />
+                <div className="carousel-caption">Image</div>
+              </div>
+              <div style={{ ...styles, backgroundColor: "aqua" }}>
+                <video
+                  className="carousel-center"
+                  controls
+                  style={{ width: "75%" }}
+                  height="250"
+                >
+                  <source
+                    src="https://www.w3schools.com/html/mov_bbb.mp4"
+                    type="video/mp4"
                   />
-                  <div className="carousel-caption">
-                    Image
-                  </div>
+                </video>
+                <div className="carousel-caption">Video</div>
+              </div>
+              <div
+                style={{
+                  height: 400,
+                  width: "100%",
+                  backgroundColor: "lightpink"
+                }}
+              >
+                <div className="carousel-center">center Text</div>
+                <div className="carousel-caption">Text</div>
+              </div>
+              <div style={{ ...styles, backgroundColor: "lightblue" }}>
+                <span>text</span>
+                <div className="carousel-caption">Text</div>
+              </div>
+              <div style={{ ...styles, backgroundColor: "lightblue" }}>
+                <div className="carousel-center">
+                  <iframe
+                    style={{ width: 500 }}
+                    height="250"
+                    src="https://www.youtube.com/embed/MhkGQAoc7bc?showinfo=0"
+                    frameBorder="0"
+                    allowFullScreen
+                  />
                 </div>
-                <div style={{height:400,width:"100%",backgroundColor:"aqua"}}>
-                  <video className="carousel-center" controls style={{width:"75%"}} height="250">
-                    <source src="https://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4"/>
-                  </video>
-                  <div className="carousel-caption">
-                    Video
-                  </div>
+                <div className="carousel-caption">Youtube</div>
+              </div>
+            </React_Bootstrap_Carousel>
+          </div>
+          <div className="col-md-12" style={{ marginTop: 20 }}>
+            <React_Bootstrap_Carousel className="carousel-fade">
+              <div style={{ ...styles, backgroundColor: "darkcyan" }}>
+                <div className="carousel-center">
+                  This carsouel transition is fade
                 </div>
-                <div style={{height:400,width:"100%",backgroundColor:"lightpink"}}>
-                  <div className="carousel-center">
-                    center Text
-                  </div>
-                  <div className="carousel-caption">
-                    Text
-                  </div>
-                </div>
-                <div style={{height:400,width:"100%",backgroundColor:"lightblue"}}>
-                  <span>
-                    text
-                  </span>
-                  <div className="carousel-caption">
-                    Text
-                  </div>
-                </div>
-                <div style={{height:400,width:"100%",backgroundColor:"lightblue"}}>
-                  <div className="carousel-center">
-                    <iframe
-                      style={{width:500}}
-                      height="250"
-                      src="https://www.youtube.com/embed/MhkGQAoc7bc?showinfo=0"
-                      frameBorder="0"
-                      allowFullScreen
-                    />
-                  </div>
-                  <div className="carousel-caption">
-                    Youtube
-                  </div>
-                </div>
-              </React_Bootstrap_Carousel>
-            </div>
+                <div className="carousel-caption">Text</div>
+              </div>
+              <div style={{ ...styles, backgroundColor: "yellowgreen" }}>
+                <span className="carousel-center">
+                  This carsouel transition is fade
+                </span>
+                <div className="carousel-caption">Text</div>
+              </div>
+            </React_Bootstrap_Carousel>
           </div>
         </div>
-      );
-    }
-};
-ReactDOM.render(
-    <Demo/>,
-    document.getElementById('Demo')
-);
+      </div>
+    );
+  }
+}
+ReactDOM.render(<Demo />, document.getElementById("Demo"));
