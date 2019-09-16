f1d90bcc74a42ccd930cb7fad9897ea3d5fb2ea8
CT Hung <skycloud1030@gmail.com>
2019-09-16 11:36:18 +0800

    Update demo to v4.0.0 with react hooks

---
+++ app/boostrap-compoent.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Row = React.memo(function Row(props) {
+  return <div className="row">{props.children}</div>;
+});
+
+const Col = React.memo(function Col(props) {
+  return (
+    <div className={`col-sm-${props.span}`} style={props.style}>
+      {props.children}
+    </div>
+  );
+});
+
+const Button = React.memo(function Button(props) {
+  const { style, bsStyle, onClick } = props;
+  const className = bsStyle ? `btn btn-${bsStyle}` : "btn";
+  return (
+    <button style={style} className={className} onClick={onClick}>
+      {props.children}
+    </button>
+  );
+});
+
+const ButtonGroup = React.memo(function ButtonGroup(props) {
+  return <div className="btn-group">{props.children}</div>;
+});
+
+/**
+ *  Boostrap Component
+ */
+export { Row, Button, Col, ButtonGroup };
