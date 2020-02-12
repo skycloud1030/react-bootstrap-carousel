e253b400d3dbfc705b1776a964d710aa29d4eac8
CT Hung <skycloud1030@gmail.com>
2020-02-12 11:30:02 +0800

    Fix window is not defined

--- app/base.js
+++ app/base.js
@@ -14,15 +14,22 @@ export default class BaseComponent extends React.PureComponent {
     !this.isUnmount && this.setState(args);
   };
   componentDidMount() {
-    window.addEventListener("beforeunload", this.unmount);
-    window.addEventListener("visibilitychange", this.visibilitychange);
+    /* istanbul ignore next */
+    if (typeof window !== "undefined") {
+      window.addEventListener("beforeunload", this.unmount);
+      window.addEventListener("visibilitychange", this.visibilitychange);
+    }
+
     typeof this.init === "function" && this.init();
   }
   componentWillUnmount() {
     this.isUnmount = true;
     typeof this.unmount === "function" && this.unmount();
-    window.removeEventListener("beforeunload", this.unmount);
-    window.removeEventListener("visibilitychange", this.visibilitychange);
+    /* istanbul ignore next */
+    if (typeof window !== "undefined") {
+      window.removeEventListener("beforeunload", this.unmount);
+      window.removeEventListener("visibilitychange", this.visibilitychange);
+    }
   }
   render() {
     return <div>The Base Component</div>;
