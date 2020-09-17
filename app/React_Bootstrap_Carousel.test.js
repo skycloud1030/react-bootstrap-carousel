165cd6a30b928168ff127ffb4b223e5896a03870
CT Hung <skycloud1030@gmail.com>
2020-09-17 15:51:59 +0800

    Edit carousel item to functional & edit test case

--- app/React_Bootstrap_Carousel.test.js
+++ app/React_Bootstrap_Carousel.test.js
@@ -1,7 +1,6 @@
 import React from "react";
-import { mount } from "enzyme";
+import { mount, shallow } from "enzyme";
 import RBCarousel from "./React_Bootstrap_Carousel.jsx";
-import RBCarouselItem from "./React_Carousel_Item.jsx";
 
 describe("React_Bootstrap_Carousel", () => {
   const carousel = mount(
@@ -11,36 +10,37 @@ describe("React_Bootstrap_Carousel", () => {
       <div>Test3</div>
     </RBCarousel>
   );
+
   const carousel2 = mount(<RBCarousel defaultActiveIndex={1} />);
   const carousel_element = carousel.instance();
   const carousel_notArray = mount(<RBCarousel>Test1</RBCarousel>);
-  const carousel_item = carousel.find(RBCarouselItem).instance();
+
   it("Should have next and prev", () => {
     expect(typeof carousel_element.slideNext).toBe("function");
     expect(typeof carousel_element.slidePrev).toBe("function");
   });
   it("Should goToSlide to corret item", () => {
-    carousel_element.goToSlide(0);
-    expect(carousel_item.props.activeIndex).toBe(0);
+    carousel.instance().goToSlide(0);
+    expect(carousel_element.state.activeIndex).toBe(0);
   });
   it("Should slidePrev to corret item", () => {
     carousel_element.goToSlide(1);
     carousel_element.slidePrev();
-    const next = carousel_item.props.activeIndex;
+    const next = carousel_element.state.activeIndex;
     expect(next).toBe(0);
   });
   it("Should slideNext to corret item", () => {
     carousel_element.goToSlide(1);
     carousel_element.slideNext();
-    const next = carousel_item.props.activeIndex;
+    const next = carousel_element.state.activeIndex;
     expect(next).toBe(2);
     carousel_element.goToSlide(2);
     carousel_element.slideNext();
-    let activeIndex = carousel_item.props.activeIndex;
+    let activeIndex = carousel_element.state.activeIndex;
     expect(activeIndex).toBe(0);
     carousel_element.goToSlide(0);
     carousel_element.slidePrev();
-    activeIndex = carousel_item.props.activeIndex;
+    activeIndex = carousel_element.state.activeIndex;
     expect(activeIndex).toBe(2);
   });
   it("Should autoplay work", () => {
@@ -50,7 +50,11 @@ describe("React_Bootstrap_Carousel", () => {
     expect(carousel2.instance().isPaused).toBe(true);
   });
   it("Should auto play when visibilitychange", () => {
-    carousel.setProps({ hidden: true, pauseOnVisibility: true, autoplay: true });
+    carousel.setProps({
+      hidden: true,
+      pauseOnVisibility: true,
+      autoplay: true,
+    });
     carousel_element.visibilitychange();
     expect(carousel.instance().isPaused).toBe(true);
     carousel.setProps({ hidden: false });
@@ -63,11 +67,11 @@ describe("React_Bootstrap_Carousel", () => {
     const carousel_element2 = carousel.instance();
     carousel_element2.goToSlide(2);
     carousel_element2.slideNext();
-    activeIndex = carousel_item.props.activeIndex;
+    activeIndex = carousel_element.state.activeIndex;
     expect(activeIndex).toBe(2);
     carousel_element2.goToSlide(0);
     carousel_element2.slidePrev();
-    activeIndex = carousel_item.props.activeIndex;
+    activeIndex = carousel_element.state.activeIndex;
     expect(activeIndex).toBe(0);
   });
   it("Should mouseOver work", () => {
@@ -84,26 +88,23 @@ describe("React_Bootstrap_Carousel", () => {
   });
   it("Should version work", () => {
     carousel.setProps({ version: 4, controls: false });
-    expect(carousel_item.props.version).toBe(4);
+    expect(carousel_element.props.version).toBe(4);
   });
   it("Should control work", () => {
     let activeIndex;
     carousel_element.goToSlide(1);
     carousel.setProps({ version: 3, controls: true, indicators: false });
     carousel.find(".left.carousel-control").simulate("click");
-    activeIndex = carousel_item.props.activeIndex;
+    activeIndex = carousel.find("React_Carousel_Item").props().activeIndex;
     expect(activeIndex).toBe(0);
     carousel.find(".right.carousel-control").simulate("click");
-    activeIndex = carousel_item.props.activeIndex;
+    activeIndex = carousel.find("React_Carousel_Item").props().activeIndex;
     expect(activeIndex).toBe(1);
   });
   it("Should indicators work", () => {
     carousel.setProps({ indicators: true, animation: false });
-    carousel
-      .find(".carousel-indicators li")
-      .first()
-      .simulate("click");
-    let activeIndex = carousel_item.props.activeIndex;
+    carousel.find(".carousel-indicators li").first().simulate("click");
+    let activeIndex = carousel.find("React_Carousel_Item").props().activeIndex;
     expect(activeIndex).toBe(0);
     carousel.unmount();
   });
--- app/React_Carousel_Item.jsx
+++ app/React_Carousel_Item.jsx
@@ -1,62 +1,60 @@
 /*React_Carousel_Item.jsx*/
 import React from "react";
 
-export default class React_Carousel_Item extends React.PureComponent {
-  static defaultProps = {
-    children: [],
-    activeIndex: 0,
-    animation: false,
-    direction: null,
-    version: 3
-  };
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    const { children, activeIndex, version, animation } = this.props;
-    const css = version == 4 ? "carousel-item" : "item";
-    let row = [];
+function React_Carousel_Item(props) {
+  const { children, activeIndex, version, animation } = props;
+  const css = version == 4 ? "carousel-item" : "item";
+  let row = [];
 
-    if (!Array.isArray(children)) {
-      return <div className={`${css} active`}>{children}</div>;
+  if (!Array.isArray(children)) {
+    return <div className={`${css} active`}>{children}</div>;
+  }
+
+  row = children.map((item, index) => {
+    let position, direction;
+    switch (true) {
+      case animation === true && index > activeIndex && version == 4:
+        position = "carousel-item-right";
+        direction = "carousel-item-next";
+        break;
+      case animation === true && index > activeIndex:
+        position = "right";
+        direction = "next";
+        break;
+      case animation === true && index < activeIndex && version == 4:
+        position = "carousel-item-left";
+        direction = "carousel-item-prev";
+        break;
+      case animation === true && index < activeIndex:
+        position = "left";
+        direction = "prev";
+        break;
+    }
+    let className = `${css} ${direction} ${position}`;
+    if (index === activeIndex) {
+      className = `${css} active`;
     }
 
-    row = children.map((item, index) => {
-      let position, direction;
-      switch (true) {
-        case animation === true && index > activeIndex && version == 4:
-          position = "carousel-item-right";
-          direction = "carousel-item-next";
-          break;
-        case animation === true && index > activeIndex:
-          position = "right";
-          direction = "next";
-          break;
-        case animation === true && index < activeIndex && version == 4:
-          position = "carousel-item-left";
-          direction = "carousel-item-prev";
-          break;
-        case animation === true && index < activeIndex:
-          position = "left";
-          direction = "prev";
-          break;
-      }
-      let className = `${css} ${direction} ${position}`;
-      if (index === activeIndex) {
-        className = `${css} active`;
-      }
-
-      return (
-        <div key={index} className={className}>
-          {item}
-        </div>
-      );
-    });
-
     return (
-      <div className="carousel-inner" role="listbox">
-        {row}
+      <div key={index} className={className}>
+        {item}
       </div>
     );
-  }
+  });
+
+  return (
+    <div className="carousel-inner" role="listbox">
+      {row}
+    </div>
+  );
 }
+
+React_Carousel_Item.defaultProps = {
+  children: [],
+  activeIndex: 0,
+  animation: false,
+  direction: null,
+  version: 3,
+};
+
+export default React.memo(React_Carousel_Item);
